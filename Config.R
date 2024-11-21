8e00349644f9773e78baa74ef56ae136d45d48e6
Gonzalez-Mirelis <genoveva@hi.no>
2024-11-21 13:58:28 +0100

    update paths

--- Config.R
+++ Config.R
@@ -1,1 +1,10 @@
-indata <- "U:\\Mareano\\VIDEOLAB\\VIDEO DATA\\200m_scale_species_by_sample\\marvid_data"
+marviddata <- "U:\\Mareano\\VIDEOLAB\\VIDEO DATA\\200m_scale_species_by_sample\\marvid_data"
+
+gisdata <- "U:\\Mareano\\VIDEOLAB\\VIDEO DATA\\200m_scale_species_by_sample\\comparison_study"
+
+ftp_url <- "ftp://ftp2.ngu.no/toGeno/EnvLayersNiN2022/all"
+
+ftp_filepath <- paste0(strsplit(ftp_url, "\\//")[[1]][1], "//havforsk:Havf8776@", 
+                       strsplit(ftp_url, "\\//")[[1]][2])#insert the credentials into the url
+
+PathTaxonary <- "U:\\Mareano\\VIDEOLAB\\VIDEO DATA\\200m_scale_species_by_sample\\Data_Delivery_2024"
